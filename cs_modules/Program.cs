cba602e22aefa0f7ec01e5f9613e7c02b4b691aa
AmirValle <valleamir460@gmail.com>
2025-03-31 11:03:36 -0300

    first commit

---
+++ cs_modules/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace GasLeakAlert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== SIMULADOR DE DETECÇÃO DE VAZAMENTOS ===");
+
+           
+            int limiteSeguro = 300; 
+            int intervaloLeitura = 5000; 
+
+            while (true)
+            {
+                int gasLevel = LerSensor();
+                Console.WriteLine($"[{DateTime.Now}] Nível de gás: {gasLevel} ppm");
+
+                if (gasLevel > limiteSeguro)
+                {
+                    EnviarAlerta(gasLevel);
+                }
+
+                System.Threading.Thread.Sleep(intervaloLeitura);
+            }
+        }
+
+        static int LerSensor()
+        {
+           
+            Random rand = new Random();
+            return rand.Next(0, 1000);
+        }
+
+        static void EnviarAlerta(int nivelGas)
+        {
+            try
+            {
+                using (var smtp = new SmtpClient("smtp.gmail.com"))
+                using (var email = new MailMessage())
+                {
+                    email.From = new MailAddress("seuemail@gmail.com");
+                    email.To.Add("alerta@empresa.com");
+                    email.Subject = "ALERTA: Vazamento de Gás Detectado!";
+                    email.Body = $"Nível crítico: {nivelGas} ppm\nData: {DateTime.Now}";
+
+                    smtp.Port = 587;
+                    smtp.Credentials = new NetworkCredential("seuemail@gmail.com", "apppassword");
+                    smtp.EnableSsl = true;
+
+                    smtp.Send(email);
+                    Console.WriteLine("ALERTA: E-mail enviado com sucesso!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERRO ao enviar alerta: {ex.Message}");
+            }
+        }
+    }
+}
